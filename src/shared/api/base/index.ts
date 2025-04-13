df54f0ab79671e0b6ac229785474197b7136ee38
Viktor Kuzubov <kuzubovvv@gmail.com>
2025-04-13 18:27:25 +0700

    save

--- src/shared/api/base/index.ts
+++ src/shared/api/base/index.ts
@@ -1,6 +1,3 @@
 export * from './apiTagsEnum'
-export * from './commonApi'
 export * from './mutationOptionsType'
-export * from './creditApi'
-export * from './userApi'
-export * from './configApi'
+export * from './api'
